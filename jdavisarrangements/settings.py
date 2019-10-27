de5889847ba98ec0763730238f4796b0a4570a79
Pierson Brannan <plbrannan95@gmail.com>
2019-10-27 10:07:51 -0400

    Render HTML

--- jdavisarrangements/settings.py
+++ jdavisarrangements/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'jdavisarrangements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- jdavisarrangements/views.py
+++ jdavisarrangements/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse('Hello from Home!')
+    # return HttpResponse('Hello from Home!')
+    return render(request, 'homepage.html')
 
 def about(request):
-    return HttpResponse('Hello there from About!')
+    # return HttpResponse('Hello there from About!')
+    return render(request, 'about.html')
