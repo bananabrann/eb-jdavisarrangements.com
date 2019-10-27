f9604fba2d5ee4862a18d67ab5e1e6c56027aaef
Pierson Brannan <plbrannan95@gmail.com>
2019-10-27 10:49:20 -0400

    Song model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    desc = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail
+    # add in song file
