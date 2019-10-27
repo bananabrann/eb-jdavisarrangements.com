cbb4cc74879dba5e24fdd2bf742dc6bd54263a04
Pierson Brannan <plbrannan95@gmail.com>
2019-10-27 14:03:26 -0400

    Display every song in db

--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Song
 
 def song_list(request):
-    return render(request, 'songs/song_list.html')
+    songs_data = Song.objects.all().order_by('date')
+    return render(request, 'songs/song_list.html', {'songs':songs_data})
